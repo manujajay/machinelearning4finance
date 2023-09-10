11df93099d4d2751b9d5440ef7029033852c414b
manujajay <manujajayawardanais@gmail.com>
2023-09-10 18:14:06 +0530

    adding latest

--- 2. unsupervised_learning_models/1. clustering.py
+++ 2. unsupervised_learning_models/1. clustering.py
@@ -1,1 +1,55 @@
-# K-Means or hierarchical clustering
+# k-means clustering
+import yfinance as yf
+import pandas_datareader.data as web
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from textwrap import wrap
+
+# Download stock data from Yahoo Finance
+tickers = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA']
+stock_data = yf.download(tickers, start='2020-01-01', end='2021-01-01')['Adj Close']
+
+# Download S&P 500 data from FRED
+sp500 = web.DataReader('SP500', 'fred', '2020-01-01', '2021-01-01')
+
+# Combine stock and S&P 500 data
+data = pd.concat([stock_data, sp500], axis=1).dropna()
+data = data.pct_change().dropna()  # Calculate daily returns
+
+# K-Means clustering
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(data)
+labels = kmeans.labels_
+
+# Create plot with subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
+
+# Main plot on ax1
+ax1.scatter(data.iloc[:, 0], data.iloc[:, 1], c=labels, cmap='viridis')
+ax1.set_title('K-Means Clustering of Stock Data and S&P 500')
+ax1.set_xlabel('AAPL Daily Returns')
+ax1.set_ylabel('GOOGL Daily Returns')
+
+# Explanation on ax2
+explanation = (
+    "Algorithm: K-Means Clustering\n"
+    "Number of Clusters: 3\n"
+    "Data: Stock prices and S&P 500 index\n\n"
+    "Explanation:\n"
+    "K-means partitions the financial data into 'K' clusters based on daily returns. "
+    "While it's not generally used for prediction, it provides valuable insights into data structure. "
+    "These insights can be instrumental for:\n\n"
+    "- Portfolio Diversification: Identifying statistically similar assets for diversification.\n"
+    "- Risk Management: Recognizing asset groups for better hedging strategies.\n"
+    "- Market Regime Identification: Understanding different market states for dynamic trading."
+)
+wrapped_explanation = "\n".join(wrap(explanation, 50))  # Wraps the text at 50 characters
+
+ax2.axis('off')
+ax2.text(0.01, 0.99, wrapped_explanation, fontsize=10, va='top', wrap=True)  # Aligns text at top left corner with a fontsize of 10
+
+# Save and show plot
+plt.tight_layout()
+plt.savefig('kmeans_financial_data_with_explanation.png')
+plt.show()
--- 2. unsupervised_learning_models/2. dimensionality_reduction.py
+++ 2. unsupervised_learning_models/2. dimensionality_reduction.py
@@ -1,1 +1,85 @@
-# PCA
+# Import necessary libraries
+import yfinance as yf
+import pandas_datareader.data as web
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from textwrap import wrap
+
+# Download stock data from Yahoo Finance
+tickers = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA']
+stock_data = yf.download(tickers, start='2020-01-01', end='2021-01-01')['Adj Close']
+
+# Download S&P 500 data from FRED
+sp500 = web.DataReader('SP500', 'fred', '2020-01-01', '2021-01-01')
+
+# Combine stock and S&P 500 data
+data = pd.concat([stock_data, sp500], axis=1).dropna()
+data = data.pct_change().dropna()  # Calculate daily returns
+
+# Apply PCA
+pca = PCA(n_components=2)
+principal_components = pca.fit_transform(data)
+
+# Analyze the components
+components_df = pd.DataFrame(pca.components_, columns=data.columns, index=[f'PC{i+1}' for i in range(2)])
+
+# Create plot with subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
+
+# Main plot on ax1
+ax1.scatter(principal_components[:, 0], principal_components[:, 1], c='blue')
+ax1.set_title('PCA of Stock Data and S&P 500')
+ax1.set_xlabel('Principal Component 1')
+ax1.set_ylabel('Principal Component 2')
+
+# Turn off axis for ax2
+ax2.axis('off')
+
+# Initial explanation
+initial_explanation = (
+    "Algorithm: Principal Component Analysis (PCA)\n"
+    "Components: 2\n"
+    "Data: Stock prices and S&P 500 index\n\n"
+    "PCA reduces the dimensionality of the data by finding new variables (Principal Components) that maximize variance."
+    "This is useful for:\n"
+    "- Data Visualization: Reducing dimensions aids in visualizing complex data.\n"
+    "- Risk Modeling: Identifying primary risk factors in a portfolio.\n"
+    "- Factor Analysis: Understanding the underlying factors affecting asset prices.\n"
+)
+
+# New section explaining the drivers of the components
+new_section = (
+    f"\nPrincipal Component 1 is most influenced by {components_df.loc['PC1'].idxmax()}.\n"
+    f"Principal Component 2 is most influenced by {components_df.loc['PC2'].idxmax()}.\n"
+)
+
+# Combine initial explanation and new section
+full_explanation = initial_explanation + new_section
+
+# Additional useful explanation
+why_useful = (
+    "\nUsefulness:\n"
+    "1. Portfolio Optimization: Identify key drivers of asset returns.\n"
+    "2. Risk Management: Uncover main risk factors.\n"
+    "3. Trading Strategies: Develop strategies based on hidden factors.\n"
+    "4. Data Visualization: Easier interpretation of high-dimensional data.\n"
+    "5. Correlation Structure: Simplify data complexity.\n"
+    "6. Market Regime Identification: Adapt trading strategies dynamically."
+)
+
+# Combine the original explanation, the 'why useful' section, and the new section
+full_explanation += why_useful
+
+wrapped_full_explanation = "\n".join(wrap(full_explanation, 50))
+ax2.text(0.01, 0.99, wrapped_full_explanation, fontsize=10, va='top', wrap=True)
+
+# Save and show plot
+plt.tight_layout()
+plt.savefig('PCA_financial_data_with_full_explanation.png')
+plt.show()
+
+# Output the components for further analysis
+print("Principal Component Analysis")
+print(components_df)
