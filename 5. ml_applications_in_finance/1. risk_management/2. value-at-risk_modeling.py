eda9f8dfb2dbd4597a000d6bcf203aebb2d42eb2
manujajay <manujajayawardanais@gmail.com>
2023-09-19 07:24:02 +0100

    adding var

--- 5. ml_applications_in_finance/1. risk_management/2. value-at-risk_modeling.py
+++ 5. ml_applications_in_finance/1. risk_management/2. value-at-risk_modeling.py
@@ -1,1 +1,89 @@
 #  Estimation of the potential losses an investment portfolio could face over a specified period for a given confidence interval.
+
+'''
+One common approach is to use a machine learning model to predict future returns, and then calculate VaR based on these predictions.
+Below is an example using a Random Forest model to predict future stock returns and subsequently calculate VaR.
+'''
+
+# Import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# Generate synthetic stock returns data
+# In a real-world application, you would fetch this data from a reliable source
+np.random.seed(42)
+n_data_points = 1000
+stock_returns = np.random.normal(0, 1, n_data_points)
+
+# Create a DataFrame
+df = pd.DataFrame(stock_returns, columns=['Returns'])
+
+# Feature engineering: use lagged returns as features
+for i in range(1, 6):
+    df[f'Lag_{i}'] = df['Returns'].shift(i)
+
+# Remove NaN
+df = df.dropna()
+
+# Split into features (X) and target (y)
+X = df.drop('Returns', axis=1)
+y = df['Returns']
+
+# Split data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and train the Random Forest model
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+print(f'Mean Squared Error: {mse}')
+
+# Calculate VaR
+confidence_level = 0.05
+VaR = np.quantile(y_pred, confidence_level)
+
+print(f'Value-at-Risk (VaR) at {confidence_level * 100}% confidence level is {VaR}')
+
+# Plot predicted returns and VaR
+plt.figure(figsize=(10, 6))
+plt.hist(y_pred, bins=30, alpha=0.75, color='blue', label='Predicted Returns')
+plt.axvline(x=VaR, color='r', linestyle='--', label=f'VaR at {confidence_level * 100}% confidence level')
+plt.xlabel('Predicted Return')
+plt.ylabel('Frequency')
+plt.title('Value-at-Risk (VaR) using Machine Learning')
+plt.legend()
+plt.show()
+
+
+'''
+Explanation:
+
+Generate Synthetic Stock Returns: The code generates synthetic stock return data for demonstration purposes.
+
+Feature Engineering: Lagged returns are used as features for the machine learning model.
+
+Train-Test Split: The data is split into training and test sets.
+
+Random Forest Model: A Random Forest Regressor model is trained on the data.
+
+Prediction: The model predicts future returns on the test set.
+
+Evaluation: The model is evaluated using Mean Squared Error (MSE).
+
+Calculate VaR: VaR is calculated based on predicted returns using the numpy quantile function.
+
+Plot: The predicted returns and VaR are plotted.
+
+The red line in the plot indicates the VaR at a 5% confidence level. According to this model, we are 95% confident that the worst daily loss will not exceed this value.
+
+Note: This is a very simplified example for demonstration purposes. In a real-world scenario, the data would be more complex, and additional steps such as data normalization, hyperparameter tuning, and validation would be necessary.
+'''
